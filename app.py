2333ab6a6bd48f083b35b6d2d55b27ebbea40df8
yashi-04 <142104444+yashi-04@users.noreply.github.com>
2025-02-01 15:08:06 +0530

    backend

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import google.generativeai as genai
 import os
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyApOoEqRjjd-5SllddXJ6AYmrhbhcJUIc0"
+os.environ["GOOGLE_API_KEY"] = "Your GEMINI API KEY"
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 app = Flask(__name__)
