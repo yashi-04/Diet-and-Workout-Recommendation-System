42e7831f5073bdb05058c4102f0ad5802cc1240d
Yashi Sharma <shyashi04@gmail.com>
2025-08-26 13:09:15 +0530

    app.py

--- app.py
+++ app.py
@@ -113,5 +113,9 @@ def recommendation():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(
+        host="0.0.0.0", 
+        port=int(os.environ.get("PORT", 5000)), 
+        debug=True
+    )
 
